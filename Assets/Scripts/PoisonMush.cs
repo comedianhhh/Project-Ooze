128b8b0efdab0dc6c60bbdcb803947fa4ce41944
comedianhhh <parentwunschsni67@gmail.com>
2022-11-08 19:16:45 +0800

    modify sprites

--- Assets/Scripts/PoisonMush.cs
+++ Assets/Scripts/PoisonMush.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoisonMush : MonoBehaviour
+public class PoisonMush : MushRoom
 {
     // Start is called before the first frame update
     void Start()
