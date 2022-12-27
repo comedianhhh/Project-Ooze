cbd7204c61648fcc8ecbbea363c0cba02f654626
comedianhhh <100120646+comedianhhh@users.noreply.github.com>
2022-12-27 23:01:25 +0800

    fix title scene's bug

--- Assets/Scripts/Manager/TimeManager.cs
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -26,19 +26,32 @@ public class TimeManager : MonoBehaviour
         gameMode = GameMode.AnimPlay;
         Application.targetFrameRate = 60;
     }
+    private void Update()
+    {
+        if (gameMode == GameMode.AnimNoPlay)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ResumeTimeLine();
 
+            }
+        }
+    }
 
     public void PauseTimeline(PlayableDirector _playableDirector)
     {
         currentPlayableDirector = _playableDirector;
         gameMode = GameMode.AnimNoPlay;
-        currentPlayableDirector.playableGraph.GetRootPlayable(0).SetSpeed(0d);
+        //currentPlayableDirector.playableGraph.GetRootPlayable(0).SetSpeed(0d);
+        currentPlayableDirector.Pause();
     }
     public void ResumeTimeLine()
     {
         Debug.Log("resumTimeline");
         gameMode = GameMode.AnimPlay;
-        currentPlayableDirector.playableGraph.GetRootPlayable(0).SetSpeed(1d);
+        //currentPlayableDirector.playableGraph.GetRootPlayable(0).SetSpeed(1d);
+        currentPlayableDirector.Play();
+
     }
 
 }
